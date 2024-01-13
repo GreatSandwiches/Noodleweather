93267054aac8757bdb7c06311490c822ab5d9a98
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-01-13 21:00:39 +1300

    More netifly backend

---
+++ fetchAndUseApiKey.js
@@ -0,0 +1,15 @@
+
+function fetchAndUseApiKey() {
+    
+    fetch("/.netlify/functions/storeApiKey")
+        .then(response => response.json())
+        .then(data => {
+            if (data.message === "API key stored successfully") {
+                // Use the API key for further processing
+                console.log("API key retrieved successfully:", data);
+            } else {
+                console.error("Failed to retrieve API key:", data.error);
+            }
+        })
+        .catch(error => console.error("Error fetching API key:", error));
+}
