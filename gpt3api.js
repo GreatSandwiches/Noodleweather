4fd3050d6fd4bd721c6685305188340323c2a6ad
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-01-23 10:49:35 +1300

    Revert "fef"
    This reverts commit 5980c2e7ab367e456bae6e14eb1ef42fea620e80.

--- gpt3api.js
+++ gpt3api.js
@@ -1,9 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   async function callchatgpt() {
-
-      console.log('Entering callchatgpt function')
-      
       const humidity = document.getElementById('humidityvalue').innerHTML;
       const conditions = document.getElementById('Conditions').innerHTML;
       const weathertemperature = document.getElementById('bigweathertemp').innerHTML;
@@ -19,7 +16,6 @@ document.addEventListener('DOMContentLoaded', () => {
               Authorization: `Bearer ${openaiSecretKey}`,
           };
 
-
           const promptSelector = document.getElementById('settingsPopup').querySelector('#promptSelector');
           const userInput = document.getElementById('settingsPopup').querySelector('#userInput');
           const responseDiv = document.getElementById('settingsPopup').querySelector('#apidata');
@@ -28,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
           
           // Pass additional parameters to getPrompt function
           const prompt = getPrompt(promptValue, location, weathertemperature, conditions, humidity, windspeed, userInput.value);
-          console.log(prompt);
+
           const data = {
               model: "gpt-3.5-turbo",
               messages: [{ "role": "user", "content": prompt }],
