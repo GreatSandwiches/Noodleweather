6bee47fdb031df59dc1abc7f3b167a84cd50916c
phishing-net <83115184+phishing-net@users.noreply.github.com>
2024-02-01 12:52:15 +1300

    Bug fixes

--- Websiteapi.js
+++ Websiteapi.js
@@ -1,4 +1,16 @@
 
+
+document.getElementById('unit-toggle').addEventListener('click', function() {
+  const button = this;
+  if (button.textContent.includes('Fahrenheit')) {
+      button.textContent = 'Switch to Celsius';
+      getWeather(location, 'imperial');
+  } else {
+      button.textContent = 'Switch to Fahrenheit';
+      getWeather(location, 'metric');
+  }
+});
+
 document.addEventListener("DOMContentLoaded", function() {
   
   const form = document.getElementById('weatherForm');
